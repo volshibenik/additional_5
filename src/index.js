230e8d6b7e47b9c025ab3daa50e084ee561262b8
volshibenik <volshibenik@gmail.com>
2017-09-16 15:48:22 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,43 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-   var pairs = [];
- 
-     
-     var twin = []; 
+     var pairs = [],
+         twin = []; 
      
      for (var i = 0; i < str.length; i++) {
+       
         if (twin.indexOf(str[i]) !==-1) {
           
           twin.splice(twin.indexOf(str[i]),1);
-     //     console.log(str[i]+'= CLO twin'+'_'+twin);
-          
           if (!isPair(str[i], pairs[pairs.length-1], bracketsConfig)) {
-      //      console.log('!!!');
             return false;
           }
           pairs.pop();
+          
         }else if (isTwin(str[i],bracketsConfig)) {
           
           twin.push(str[i]);
-      //    console.log(str[i]+'=twin'+' OPE '+twin);
           pairs.push(str[i]);
           
         }else if (isOpening(str[i], bracketsConfig)) {
           
-       //   console.log(str[i]+' OPE ');
           pairs.push(str[i]);
           
         } else {
           
- //         console.log(str[i]+' CLO ');
           if (!isPair(str[i], pairs[pairs.length-1], bracketsConfig)) {
-        //    console.log('!!!');
             return false;
           }
           pairs.pop();
           
         }
      }
-//     console.log(pairs);
+
      return pairs.length === 0;
   
 }
@@ -70,6 +63,3 @@ function isPair(symbol, goal, arr) {
   return false;
 }
 
-
-    
-    
